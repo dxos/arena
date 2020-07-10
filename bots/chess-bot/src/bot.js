1be37f4fe75b4c5aa3d4e97e0dccca437ad6feb6
egorgripasov <gripasovegor@gmail.com>
2020-07-10 16:46:22 -0400

    Use updated deps.

--- bots/chess-bot/src/bot.js
+++ bots/chess-bot/src/bot.js
@@ -9,19 +9,19 @@ import { ChessModel } from '@dxos/chess-core';
  * Chess bot.
  */
 export class ChessBot extends Bot {
-  constructor (config) {
-    super(ChessModel, config);
-  }
+  // constructor (config) {
+  //   super(ChessModel, config);
+  // }
 
-  // eslint-disable-next-line class-methods-use-this
-  async onModelUpdate (model) {
-    if (model.game.turn() === 'b') {
-      console.log('This is a black move, making a move...');
-      const moves = model.game.moves({ verbose: true });
-      if (moves.length > 0) {
-        const move = moves[Math.floor(Math.random() * moves.length)];
-        model.makeMove(move);
-      }
-    }
-  }
+  // // eslint-disable-next-line class-methods-use-this
+  // async onModelUpdate (model) {
+  //   if (model.game.turn() === 'b') {
+  //     console.log('This is a black move, making a move...');
+  //     const moves = model.game.moves({ verbose: true });
+  //     if (moves.length > 0) {
+  //       const move = moves[Math.floor(Math.random() * moves.length)];
+  //       model.makeMove(move);
+  //     }
+  //   }
+  // }
 }
