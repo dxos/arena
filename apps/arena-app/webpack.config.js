12f628b0783d7047f14ef8f65c9385c5e6617422
rzadp <przemek@ethworks.io>
2020-07-10 15:19:40 +0200

    SVG import fix

--- apps/arena-app/webpack.config.js
+++ apps/arena-app/webpack.config.js
@@ -146,7 +146,7 @@ module.exports = {
 
       // fonts
       {
-        test: /\.(woff(2)?|ttf|eot|svg|png|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|png|jpg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
