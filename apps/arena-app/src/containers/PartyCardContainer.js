f1552a470a661086f4e6a5b9091040d3b3ae00f9
rzadp <przemek@ethworks.io>
2020-07-16 12:24:42 +0200

    Bump SDK

--- apps/arena-app/src/containers/PartyCardContainer.js
+++ apps/arena-app/src/containers/PartyCardContainer.js
@@ -7,13 +7,24 @@ import React from 'react';
 import { keyToString } from '@dxos/crypto';
 
 import { useViews } from '../model';
-import { PartyCard } from '@dxos/react-appkit';
+import { PartyCard, useAppRouter, usePads } from '@dxos/react-appkit';
+import { useClient } from '@dxos/react-client';
 
 const PartyCardContainer = ({ party }) => {
+  const client = useClient();
+  const router = useAppRouter();
+  const [pads] = usePads();
   const topic = keyToString(party.publicKey);
   const { model, createView } = useViews(topic);
   return (
-    <PartyCard party={party} viewModel={model} createView={createView} />
+    <PartyCard
+      party={party}
+      viewModel={model}
+      createView={createView}
+      client={client}
+      router={router}
+      pads={pads}
+    />
   );
 };
 
