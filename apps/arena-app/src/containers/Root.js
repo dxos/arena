d41401c88f4dec45e8f0b045346a2e526627b504
Przemyslaw Rzad <przemek@ethworks.io>
2020-07-17 14:55:58 +0200

    View Settings update (#43)
    * New viewsettings
    
    * Bump SDK
    
    * Lint
    
    * Bump sdk

--- apps/arena-app/src/containers/Root.js
+++ apps/arena-app/src/containers/Root.js
@@ -14,8 +14,7 @@ import {
   Registration,
   RequireWallet,
   SystemRoutes,
-  Theme,
-  ViewSettingsContainer
+  Theme
 } from '@dxos/react-appkit';
 import { ClientContextProvider } from '@dxos/react-client';
 import GamePad from '@dxos/game-pad';
@@ -56,9 +55,6 @@ const Root = ({ config }) => {
                 <RequireWallet redirect={routes.register}>
                   <Switch>
                     {SystemRoutes(router)}
-                    <Route exact path="/settings/:topic?"><Redirect to="/home" /></Route>
-                    <Route exact path={'/settings/:topic([0-9a-fA-F]{64})/:item'} component={ViewSettingsContainer} />
-
                     <Route exact path="/app/:topic?"><Redirect to="/home" /></Route>
                     <Route exact path={routes.app} component={App} />
 
