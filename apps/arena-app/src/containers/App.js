9cdc2303d7a6d297784cf220dfc8f6806dbeab09
rzadp <przemek@ethworks.io>
2020-07-17 14:19:21 +0200

    New viewsettings

--- apps/arena-app/src/containers/App.js
+++ apps/arena-app/src/containers/App.js
@@ -2,17 +2,18 @@
 // Copyright 2020 DXOS.org
 //
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Home, BuildOutlined } from '@material-ui/icons';
+import { Home } from '@material-ui/icons';
 import IconButton from '@material-ui/core/IconButton';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 import { noop } from '@dxos/async';
 import { keyToBuffer } from '@dxos/crypto';
 import { useClient } from '@dxos/react-client';
-import { AppContainer, usePads, useAppRouter, DefaultViewSidebar, useViews } from '@dxos/react-appkit';
+import { AppContainer, usePads, useAppRouter, DefaultViewSidebar, useViews, ViewSettingsDialog } from '@dxos/react-appkit';
 
 const useStyles = makeStyles(theme => ({
   main: {
@@ -37,6 +38,7 @@ const App = () => {
   const { model } = useViews(topic);
   const item = model.getById(viewId);
   const client = useClient();
+  const [viewSettingsOpen, setViewSettingsOpen] = useState(false);
 
   const pad = item ? pads.find(pad => pad.type === item.type) : undefined;
 
@@ -52,20 +54,32 @@ const App = () => {
       <Home onClick={() => router.push({ path: '/home' })} />
     </IconButton>
     <IconButton color="inherit">
-      <BuildOutlined onClick={() => router.push({ path: '/settings', topic, item: viewId })} />
+      <SettingsIcon onClick={() => setViewSettingsOpen(true)} />
     </IconButton>
 
   </>);
 
   return (
-    <AppContainer
-      appBarContent={appBarContent}
-      sidebarContent={<DefaultViewSidebar />}
-    >
-      <div className={classes.main}>
-        {pad && <pad.main topic={topic} viewId={viewId} />}
-      </div>
-    </AppContainer>
+    <>
+      <AppContainer
+        appBarContent={appBarContent}
+        sidebarContent={<DefaultViewSidebar />}
+      >
+        <div className={classes.main}>
+          {pad && <pad.main topic={topic} viewId={viewId} viewSettingsOpen={viewSettingsOpen} setViewSettingsOpen={setViewSettingsOpen} />}
+        </div>
+      </AppContainer>
+      {pad && !pad.customViewSettings && (
+        <ViewSettingsDialog
+          open={viewSettingsOpen}
+          onClose={() => setViewSettingsOpen(false)}
+          viewModel={model}
+          pads={pads}
+          viewId={viewId}
+        />
+      )}
+    </>
+
   );
 };
 
