b37fa9d45af1ae10055891fae3e8097353e313f6
rzadp <przemek@ethworks.io>
2020-07-24 11:39:03 +0200

    Comment out react refresh plugin

--- apps/arena-app/webpack.config.js
+++ apps/arena-app/webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 const VersionFile = require('webpack-version-file-plugin');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+// const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const { ConfigPlugin } = require('@dxos/config/ConfigPlugin');
 
@@ -102,9 +102,9 @@ module.exports = {
       templateParameters: {
         title: 'Arena'
       }
-    }),
+    })
 
-    isDevelopment && new ReactRefreshWebpackPlugin({ overlay: false })
+    // isDevelopment && new ReactRefreshWebpackPlugin({ overlay: false })
   ].filter(Boolean),
 
   module: {
@@ -130,7 +130,7 @@ module.exports = {
               '@babel/plugin-proposal-nullish-coalescing-operator',
               '@babel/plugin-proposal-optional-chaining',
               'babel-plugin-styled-components',
-              isDevelopment && require.resolve('react-refresh/babel')
+              // isDevelopment && require.resolve('react-refresh/babel')
             ].filter(Boolean)
           }
         }
