b5c02b829c71b4c1146c9daa6ca480bd431c521a
rzadp <przemek@ethworks.io>
2020-08-13 14:21:19 +0200

    Remove obsolete code from chess-pad

--- pads/chess-pad/src/model.js
+++ pads/chess-pad/src/model.js
@@ -6,68 +6,13 @@ import assert from 'assert';
 import Chess from 'chess.js';
 
 import { TYPE_CHESS_GAME, TYPE_CHESS_MOVE, TYPE_CHESS_PLAYERSELECT, ChessModel } from '@dxos/chess-core';
-import { createId } from '@dxos/crypto';
 import { useModel } from '@dxos/react-client';
-
-// TODO(burdon): Define types.
-
-/**
- * Provides item list and item creator.
- * @param {string} topic
- * @returns {[Object[], function]}
- */
-export const useItemList = (topic) => {
-  const model = useModel({ options: { type: TYPE_CHESS_GAME, topic } });
-
-  // TODO(burdon): CRDT.
-  const messages = model?.messages ?? [];
-  const items = Object.values(messages.reduce((map, item) => {
-    map[item.itemId] = item;
-    return map;
-  }, {}));
-
-  return {
-    items,
-    createItem: opts => {
-      const itemId = createId();
-      model.appendMessage({ __type_url: TYPE_CHESS_GAME, itemId, ...opts });
-      return itemId;
-    },
-    renameItem: (itemId, title) => {
-      model.appendMessage({ __type_url: TYPE_CHESS_GAME, itemId, title });
-    }
-  };
-};
-
-/**
- * Provides item metadata and updater.
- * @param {string} topic
- * @param {string} itemId
- * @returns {[{title}, function]}
- */
-export const useItem = (topic, itemId) => {
-  const model = useModel({ options: { type: TYPE_CHESS_GAME, topic, itemId } });
-  if (!model) {
-    return [[]];
-  }
-
-  // TODO(burdon): CRDT.
-  const { messages = [] } = model;
-  const { title } = messages.length > 0 ? messages[messages.length - 1] : {};
-
-  return [
-    { title },
-    ({ title }) => {
-      model.appendMessage({ __type_url: TYPE_CHESS_GAME, itemId, title });
-    }
-  ];
-};
 
 /**
  * Provides game state and a way to make moves.
  * @param topic
  * @param itemId
- * @returns {[Object[], function]}
+ * @returns {[Object[], function, Object]}
  */
 export const useChessModel = (topic, itemId) => {
   assert(topic);
