95854e3aff816f4422b8bc3c85eedaf05e5e9f7c
egorgripasov <gripasovegor@gmail.com>
2020-10-16 13:59:48 -0400

    cli-chess new echo updates.

--- packages/cli-chess/src/index.js
+++ packages/cli-chess/src/index.js
@@ -2,16 +2,26 @@
 // Copyright 2020 DXOS.org
 //
 
+import { TYPE_CHESS_GAME, ChessModel } from '@dxos/chess-core';
 import { createCLI } from '@dxos/cli-core';
+import { createModelAdapter } from '@dxos/model-adapter';
 
 import info from '../extension.yml';
 import { ChessModule } from './modules/chess';
+
+const initGameModel = async (state) => {
+  const model = createModelAdapter(TYPE_CHESS_GAME, ChessModel);
+
+  state.models.push(model);
+  state.chessModel = model;
+};
 
 module.exports = createCLI(
   {
     modules: [ChessModule],
     dir: __dirname,
     main: !module.parent,
+    init: initGameModel,
     info
   }
 );
