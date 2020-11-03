a7121fccb94227ae5c515fadfd8edb330a845411
rzadp <roopert7@gmail.com>
2020-11-03 12:40:33 +0100

    First playable CLI version

--- packages/game-cli/src/modules/game.js
+++ packages/game-cli/src/modules/game.js
@@ -6,6 +6,7 @@ import assert from 'assert';
 import Chance from 'chance';
 
 import { asyncHandler } from '@dxos/cli-core';
+import { createId } from '@dxos/crypto';
 import { log } from '@dxos/debug';
 import { GameModel, GAME_TYPE_GAME, GAME_TYPE_MOVE } from '@dxos/game-model';
 import { ObjectModel } from '@dxos/object-model';
@@ -14,15 +15,15 @@ const chance = new Chance();
 
 const generateRandom = () => chance.sentence({ words: 1 });
 
-const gameUpdateHandler = model => {
-  const { game } = model.state;
+const gameUpdateHandler = gameModel => {
+  const { game } = gameModel.model;
   log(`\n\n${game.ascii()}\n`);
   if (game.isOver()) {
     log(`Game over. Result: ${game.result()}\n`);
   }
 };
 
-export const GameModule = ({ getClient, stateManager }) => ({
+export const GameModule = ({ stateManager }) => ({
   command: ['$0', 'game'],
   describe: 'Game CLI.',
   builder: yargs => yargs
@@ -33,10 +34,9 @@ export const GameModule = ({ getClient, stateManager }) => ({
       builder: yargs => yargs,
 
       handler: asyncHandler(async () => {
-        const topic = stateManager.currentParty;
-        assert(topic, 'Invalid party.');
-
         const party = stateManager.party;
+        assert(party, 'Invalid party.');
+
         const result = party.database.queryItems({ type: GAME_TYPE_GAME });
         const games = result.value.map(record => ({ id: record.id, name: record.model.getProperty('title') || 'untitled' }));
         return JSON.stringify(games);
@@ -52,15 +52,20 @@ export const GameModule = ({ getClient, stateManager }) => ({
       handler: asyncHandler(async argv => {
         const { title = generateRandom() } = argv;
 
-        const topic = stateManager.currentParty;
-        assert(topic, 'Invalid party.');
         const party = stateManager.party;
+        assert(party, 'Invalid party.');
 
         const createdTitle = title || 'untitled';
         const game = await party.database.createItem({
           type: GAME_TYPE_GAME,
           model: ObjectModel,
           props: { title: createdTitle }
+        });
+
+        await party.database.createItem({
+          model: GameModel,
+          type: GAME_TYPE_MOVE,
+          parent: game.id
         });
 
         return JSON.stringify({ id: game.id, title: createdTitle });
@@ -76,12 +81,17 @@ export const GameModule = ({ getClient, stateManager }) => ({
       handler: asyncHandler(async argv => {
         const { gameId } = argv;
 
-        const topic = stateManager.currentParty;
-        assert(topic, 'Invalid party.');
+        const party = stateManager.party;
+        assert(party, 'Invalid party.');
 
-        const client = await getClient();
-        const gameModel = await client.modelFactory.createModel(GameModel, { type: GAME_TYPE_MOVE, topic, gameId });
-        await stateManager.setModel(gameModel, gameUpdateHandler);
+        const gameItem = party.database.getItem(gameId);
+        assert(gameItem, 'Invalid game.');
+
+        const gameModel = gameItem.children[0];
+        assert(gameModel, 'Corrupted game model');
+
+        await stateManager.setItem(gameModel, gameUpdateHandler);
+        gameUpdateHandler(gameModel);
 
         return JSON.stringify({ gameId });
       })
@@ -92,19 +102,23 @@ export const GameModule = ({ getClient, stateManager }) => ({
       describe: 'Move piece.',
       builder: yargs => yargs
         .option('position', { type: 'string' })
-        .option('piece', { type: 'number' }),
+        .option('piece', { type: 'string' }),
 
       handler: asyncHandler(async argv => {
         const { position, piece } = argv;
 
-        const gameModel = stateManager.model;
+        const gameModel = stateManager.item;
         assert(gameModel, 'Invalid game.');
 
-        const move = gameModel.state.game.move + 1;
-        gameModel.appendMessage({
-          __type_url: GAME_TYPE_MOVE,
+        const playedPiece = ['o', 'x'].includes(piece) ? ['o', 'x'].indexOf(piece) : parseInt(piece);
+
+        assert(playedPiece === 0 || playedPiece === 1, 'Invalid piece played');
+
+        const move = gameModel.model.game.move + 1;
+        gameModel.model.move({
+          id: createId(),
           position,
-          piece,
+          piece: playedPiece,
           move
         });
       })
