b4865b63b911038a50068c6e049b1f32c1b2efe1
Egor Gripasov <gripasovegor@gmail.com>
2020-12-03 15:59:12 +0300

    Bot deploy test. (#115)
    * Test bot deployment.
    
    * Update yarn.lock

---
+++ scripts/deploy_bots_to_wns.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+for botdir in `find ./bots -name 'chess-bot' -type d | grep -v node_modules`; do
+  pushd $botdir
+
+  WNS_ORG="${WNS_ORG:-egorgripasov}"
+  PKG_CHANNEL="${PKG_CHANNEL:-}"
+  PKG_NAME=`cat package.json | jq -r '.name' | cut -d'/' -f2- | sed 's/-bot$//'`
+  WNS_NAME="$WNS_ORG/$PKG_NAME"
+  WNS_ARCH="${WNS_ARCH:-linux-x64}"
+  
+  cat <<EOF > bot.yml
+name: $PKG_NAME
+EOF
+
+  cat bot.yml
+  echo "wrn://${WNS_ORG}/bot/${PKG_NAME}${PKG_CHANNEL}"
+
+  yarn package:${WNS_ARCH}
+  yarn -s wire bot publish
+  yarn -s wire bot register --name "wrn://egorgripasov/bot/${PKG_NAME}${PKG_CHANNEL}"
+
+  popd
+done
