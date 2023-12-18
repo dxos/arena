f408073e1877b32f6927723ca8284435af77dd7e
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-12-18 13:14:11 -0800

    ⚡️ `atomWithStorage`

---
+++ src/lib/atomWithStorage.ts
@@ -0,0 +1,42 @@
+import { Atom, atom, react } from "signia";
+
+// Inspired by the Jotai Utility
+// https://github.com/pmndrs/jotai/pull/394
+
+type Storage<Value> = {
+  getItem: (key: string) => Value;
+  setItem: (key: string, newValue: Value) => void;
+};
+
+const defaultStorage: Storage<unknown> = {
+  getItem: (key) => {
+    const storedValue = localStorage.getItem(key);
+    if (storedValue === null) {
+      throw new Error("no value stored");
+    }
+    return JSON.parse(storedValue);
+  },
+  setItem: (key, newValue) => {
+    localStorage.setItem(key, JSON.stringify(newValue));
+  },
+};
+
+export function atomWithStorage<Value>(
+  key: string,
+  initialValue: Value,
+  storage: Storage<Value> = defaultStorage as Storage<Value>
+): Atom<Value> {
+  const getInitialValue = () => {
+    try {
+      return storage.getItem(key);
+    } catch {
+      return initialValue;
+    }
+  };
+
+  const baseAtom = atom(key, getInitialValue());
+
+  react(`${key}__reactor`, () => storage.setItem(key, baseAtom.value));
+
+  return baseAtom;
+}
