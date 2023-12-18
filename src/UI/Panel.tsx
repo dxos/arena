88e4e11a50748e703875de979f7bd3a07ed898cb
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-12-18 11:10:24 -0800

    💅 Visual: Simplify Panel shadow

--- src/UI/Panel.tsx
+++ src/UI/Panel.tsx
@@ -3,8 +3,10 @@ import { cn } from "../lib";
 
 // TODO: Move this to a theme file
 const customPanelStyles: CSSProperties = {
-  boxShadow:
-    "0 0 0 1px rgba(53,72,91,.14), 0 2.75px 2.21px rgba(0,0,0,.07), 0 6.65px 5.32px rgba(0,0,0,.043), 0 12.5px 10px rgba(0,0,0,.03), 0 22px 18px rgba(0,0,0,.03), 0 42px 33.4px rgba(0,0,0,.02), 0 100px 80px rgba(0,0,0,.017)",
+  boxShadow: `
+    0 0 0 1px rgba(53,72,91,.14),
+    0 2.75px 2.21px rgba(0,0,0,.07),
+    0 22px 18px rgba(0,0,0,.01)`,
   backgroundImage: "url('/images/noise.png')",
   backgroundSize: "80px 80px",
 };
