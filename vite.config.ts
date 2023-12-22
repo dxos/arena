f6b4f4115f7281c2b807bcba80d0fe9b2019e5fa
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-12-21 17:05:10 -0800

    🏗️ Fix the build

--- vite.config.ts
+++ vite.config.ts
@@ -11,15 +11,26 @@ export default defineConfig({
   server: {
     host: true,
   },
-  build: { outDir: "out/dxos-templates", target: "esnext" },
-  optimizeDeps: { esbuildOptions: { target: "esnext" } },
-  define: {
-    process: {
-      env: {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+  build: {
+    target: "esnext",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+      },
+      output: {
+        // Generate nicer chunk names. Default makes most chunks have names like index-[hash].js.
+        chunkFileNames,
+        manualChunks: {
+          react: ["react", "react-dom"],
+          dxos: ["@dxos/react-client"],
+          ui: ["@dxos/react-ui", "@dxos/react-ui-theme"],
+        },
       },
     },
   },
+  optimizeDeps: { esbuildOptions: { target: "esnext" } },
+
   plugins: [
     VaultPlugin(),
     ConfigPlugin(),
@@ -31,23 +42,28 @@ export default defineConfig({
         resolve(__dirname, "./src/**/*.{js,ts,jsx,tsx}"),
         resolve(__dirname, "node_modules/@dxos/react-ui/dist/**/*.mjs"),
         resolve(__dirname, "node_modules/@dxos/react-ui-theme/dist/**/*.mjs"),
-        resolve(
-          __dirname,
-          "./node_modules/@braneframe/plugin-*/dist/lib/**/*.mjs"
-        ),
-        resolve(
-          __dirname,
-          "./node_modules/@dxos/react-ui-mosaic/dist/lib/**/*.mjs"
-        ),
-        resolve(
-          __dirname,
-          "./node_modules/@dxos/react-ui-stack/dist/lib/**/*.mjs"
-        ),
-        resolve(
-          __dirname,
-          "./node_modules/@dxos/react-ui-navtree/dist/lib/**/*.mjs"
-        ),
+        resolve(__dirname, "./node_modules/@braneframe/plugin-*/dist/lib/**/*.mjs"),
+        resolve(__dirname, "./node_modules/@dxos/react-ui-mosaic/dist/lib/**/*.mjs"),
+        resolve(__dirname, "./node_modules/@dxos/react-ui-stack/dist/lib/**/*.mjs"),
+        resolve(__dirname, "./node_modules/@dxos/react-ui-navtree/dist/lib/**/*.mjs"),
       ],
     }),
   ],
 });
+
+function chunkFileNames(chunkInfo) {
+  if (chunkInfo.facadeModuleId && chunkInfo.facadeModuleId.match(/index.[^\/]+$/gm)) {
+    let segments = chunkInfo.facadeModuleId.split("/").reverse().slice(1);
+    const nodeModulesIdx = segments.indexOf("node_modules");
+    if (nodeModulesIdx !== -1) {
+      segments = segments.slice(0, nodeModulesIdx);
+    }
+    const ignoredNames = ["dist", "lib", "browser"];
+    const significantSegment = segments.find((segment) => !ignoredNames.includes(segment));
+    if (significantSegment) {
+      return `assets/${significantSegment}-[hash].js`;
+    }
+  }
+
+  return "assets/[name]-[hash].js";
+}
